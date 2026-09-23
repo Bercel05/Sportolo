919a47442fed2273aa827594f7e65d08f8e9c2f4
Bercel05 <markusb@kkszki.hu>
2026-09-23 09:20:27 +0200

    Second exercise is ready

--- Controllers/EredmenyekController.cs
+++ Controllers/EredmenyekController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sportolo.Models;
+using MySqlConnector;
+using Sportolo.Models.DTOs;
 
 namespace Sportolo.Controllers
 {
@@ -17,6 +19,32 @@ namespace Sportolo.Controllers
             connector.Open();
             string sql = "SELECT * FROM `eredmeny` WHERE 1";
             var cmd = new MySqlCommand(sql, connector);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var eredmeny = new Eredmenyek
+                {
+                    id = dr.GetInt32("Id"),
+                    competition = dr.GetString("competition"),
+                    description = dr.GetString("description"),
+                    resultTime = dr.GetDateTime("resultTime"),
+                    updateTime = dr.GetDateTime("updateTime"),
+                    sportoloId = dr.GetInt32("sportoloId"),
+                };
+                eredmenyek.Add(eredmeny);
+            }
+            connector.Close();
+            return eredmenyek;
+        }
+        [HttpGet("EredmenyGet")]
+        public object EredmenyGet(int id)
+        {
+            var connector = new MySqlConnection(ConnectionString);
+            List<Eredmenyek> eredmenyek = new();
+            connector.Open();
+            var sql = $"SELECT * FROM `eredmeny` WHERE id = @id";
+            var cmd = new MySqlCommand(sql, connector);
+            cmd.Parameters.AddWithValue("@id", id);
             var dr = cmd.ExecuteReader();
             while (dr.Read())
             {
